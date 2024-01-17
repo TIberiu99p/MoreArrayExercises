0f0d419f40217ed7f25c46f53a6f936fd237ff26
Tiberius <35663392+TIberiu99p@users.noreply.github.com>
2024-01-17 14:58:25 +0000

    Add files via upload

---
+++ TestMoreArrayExercises/MoreArrayExercises.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MoreArrayExercises.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoreArrayExercises.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92cbcc70-14ca-4a8a-9531-069e322bcad4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestMoreArrayExercises/MoreArrayExercises.Tests/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace MoreArrayExercises.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private int[] arr;
+        private int size;
+        private int value;
+        [SetUp]
+        public void Setup() 
+        { 
+            //arange
+            arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 20 };
+            size = arr.Length;
+            value = 8;
+        }
+
+        [Test]
+        public void TestSumArray()
+        {   
+            
+            //act
+            int result = Program.SumArray(arr);
+            //assert
+            Assert.That(result, Is.EqualTo(210));
+
+        }
+
+        [Test]
+        public void TestSequentialSearch()
+        {
+            //act
+            int result = Program.SequnatialSearch(arr, size, value);
+            //assert
+            Assert.That(result, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void TestBinarySearch()
+        {
+
+            //act
+            int result = Program.BinarySearch(arr, size, value);
+            //assert
+            Assert.That(result, Is.EqualTo(7));
+        }
+    }
+}
